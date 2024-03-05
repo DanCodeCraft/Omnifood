47aed68d7f21809182df08f47187296525d62766
DanCodeCraft <160060194+DanCodeCraft@users.noreply.github.com>
2024-03-04 19:42:35 -0500

    Add files via upload

--- Omnifood-Desktop/JavaScript/script.js
+++ Omnifood-Desktop/JavaScript/script.js
@@ -29,6 +29,33 @@ btnNavEl.addEventListener("click", function () {
 });
 
 ///////////////////////////////////////////////////////////
+// STICKY NAVIGATION
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
 // SMOOTH SCROLLING ANIMATION
 
 const allLinks = document.querySelectorAll("a:link");
